fe78f8d94fe2aa3dde9458884f9f5aa5a4f50fa9
William Ma <william.ma@doordash.com>
2020-12-01 22:06:24 -0800

    Other binary search implementation.

--- Common/binarySearch.py
+++ Common/binarySearch.py
@@ -14,3 +14,20 @@ def binarySearch(a, target):
             l = c
             c = (c + h) // 2
     return c
+
+
+def binarySearch2(a, target):
+    # Returns lower of value if cannot be found.
+    l = 0
+    while l <= h:
+        m = (l + h) // 2
+        s = m * m
+        if s == num:
+            return True
+
+        # Go lower case.
+        elif s > num:
+            h = m - 1
+        # Go higher case.
+        else:
+            l = m + 1
