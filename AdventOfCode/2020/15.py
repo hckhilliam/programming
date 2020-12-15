92be54679de4e2f7bfc7944f4945b2fc9e63449c
William <mawilliamyan@gmail.com>
2020-12-14 21:48:19 -0800

    2020 AoC day 15.

---
+++ AdventOfCode/2020/15.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+import functools
+import math
+
+
+def part1(data):
+    data = [int(i) for i in parse_data(data)[0].split(',')]
+    d = {k: i + 1 for i, k in enumerate(data[:-1])}
+    num = data[-1]
+    turn = len(data)
+
+    while turn < 2020:
+        if num not in d:
+            d[num] = turn
+            num = 0
+        else:
+            t = turn - d[num]
+            d[num] = turn
+            num = t
+        turn += 1
+    return num
+
+
+def part2(data):
+    data = [int(i) for i in parse_data(data)[0].split(',')]
+    d = {k: i + 1 for i, k in enumerate(data[:-1])}
+    num = data[-1]
+    turn = len(data)
+
+    while turn < 30000000:
+        if num not in d:
+            d[num] = turn
+            num = 0
+        else:
+            t = turn - d[num]
+            d[num] = turn
+            num = t
+        turn += 1
+    return num
+
+
+def parse_data(data):
+    rows = data.split('\n')
+    return rows
