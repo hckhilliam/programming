a668d36502f6c9e537758bc156eeb3c00ec855e8
hckhilliam <mawilliamyan@gmail.com>
2023-12-20 17:34:06 -0500

    Improve main runner for aoc.

--- AdventOfCode/main.py
+++ AdventOfCode/main.py
@@ -2,13 +2,17 @@
 import runner
 import sys
 
-part = None
+day = 1
 if len(sys.argv) > 1:
-    part = int(sys.argv[1])
+    day = int(sys.argv[1])
+
+part = None
+if len(sys.argv) > 2:
+    part = int(sys.argv[2])
 
 is_test = 0
-if len(sys.argv) > 2:
-    is_test = int(sys.argv[2])
+if len(sys.argv) > 3:
+    is_test = int(sys.argv[3])
 
 
-runner.run(day=20, year=2023, part=part, is_test=is_test)
+runner.run(day=day, year=2023, part=part, is_test=is_test)
